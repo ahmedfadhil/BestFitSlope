3a8aaab58528a4d8ba3bb72f6980a9840b33dc0d
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-07-17 23:45:06 +0200

    new dataset created and correlation plotted

--- LinearRegression.py
+++ LinearRegression.py
@@ -6,11 +6,23 @@ import random
 
 style.use('fivethirtyeight')
 
-xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
-ys = np.array([3, 4, 2, 6, 7, 8], dtype=np.float64)
+
+# xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+# ys = np.array([3, 4, 2, 6, 7, 8], dtype=np.float64)
 
 
-def creat_dataset(hm, variance, step=2, correlation=False):
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+        xs = [i for i in range(len(ys))]
+
     return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 
@@ -37,6 +49,7 @@ def coefficient_of_determination(ys_origin, ys_line):
     return 1 - (squareed_error_regr / squareed_error_y_mean)
 
 
+xs, ys = create_dataset(40, 40, 2, correlation='pos')
 m, b = best_fit_slope_and_intersept(xs, ys)
 
 print(m, b)
@@ -53,6 +66,7 @@ r_squared = coefficient_of_determination(ys, regression_line)
 print(r_squared)
 
 plt.scatter(xs, ys)
-plt.scatter(predict_x, predict_y, color='g')
 plt.plot(xs, regression_line)
+plt.scatter(predict_x, predict_y, color='g',s=100)
+
 plt.show()
